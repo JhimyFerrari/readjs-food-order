ba73690939b5bbac24dba7efbef5f6d156cbce06
Jhimy Ferrari <souzaferrarik@gmail.com>
2023-07-31 17:40:12 -0300

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
-
+import Card from './components/Cart/Cart';
 function App() {
   return (
     <CartProvider>
+      <Cart/>
       <Header />
       <main>
         <Meals />
